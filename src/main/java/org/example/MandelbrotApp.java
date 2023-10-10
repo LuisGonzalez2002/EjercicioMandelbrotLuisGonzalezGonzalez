e0360b4ba2dff340082ce076cf19c890ce0cc182
luisglez <luis.gonzalez2002419@gmail.com>
2023-10-10 11:08:02 +0200

    v2.0

--- src/main/java/org/example/MandelbrotApp.java
+++ src/main/java/org/example/MandelbrotApp.java
@@ -1,2 +1,152 @@
-package org.example;public class MandelbrotApp {
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.*;
+
+public class MandelbrotApp extends JFrame{
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    private JPanel mandelbrotPanel;
+    private JSpinner workerSpinner;
+    private ExecutorService executorService;
+    private JButton repaintButton;
+    private boolean isRepainting = false;
+
+
+
+    public MandelbrotApp() {
+        setTitle("Conjunto de Mandelbrot");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mandelbrotPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                renderMandelbrot(g);
+            }
+        };
+
+        workerSpinner = new JSpinner(new SpinnerNumberModel(5, 1, 16, 1));
+
+        workerSpinner.addChangeListener(e -> restartCalculation());
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(new JLabel("Número de Trabajadores:"));
+        controlPanel.add(workerSpinner);
+
+        add(controlPanel, BorderLayout.NORTH);
+        add(mandelbrotPanel, BorderLayout.CENTER);
+
+        executorService = Executors.newCachedThreadPool();
+
+        repaintButton = new JButton("Repintar");
+        repaintButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!isRepainting) {
+                    isRepainting = true;
+                    repaintButton.setEnabled(false);
+                    setBlackBackground();
+                }
+            }
+        });
+
+        controlPanel.add(repaintButton);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MandelbrotApp app = new MandelbrotApp();
+            app.setVisible(true);
+        });
+    }
+
+    private void renderMandelbrot(Graphics g) {
+        int numWorkers = (int) workerSpinner.getValue();
+        int width = mandelbrotPanel.getWidth();
+        int height = mandelbrotPanel.getHeight();
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        //Aqui lo que se está haciendo es dividir la altura de la imagen entre el número de trabajadores
+        //para que cada uno se encargue de una parte de la imagen
+        int stripHeight = height / numWorkers;
+
+        //Utilidad de Java para poder obtener los resultados de los trabajadores de manera paralela,
+        // no esperamos valor de retorno por ello se devuelve un void
+        CompletionService<Void> completionService = new ExecutorCompletionService<>(executorService);
+
+        for (int i = 0; i < numWorkers; i++) {
+            int startY = i * stripHeight;
+            int endY = (i == numWorkers - 1) ? height : startY + stripHeight;
+            MandelbrotWorker worker = new MandelbrotWorker(image, startY, endY);
+            completionService.submit(worker);
+        }
+
+        // Esperar a que todos los trabajadores terminen
+        for (int i = 0; i < numWorkers; i++) {
+            try {
+                completionService.take().get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        g.drawImage(image, 0, 0, null);
+    }
+
+    private void restartCalculation() {
+        executorService.shutdown();
+        executorService = Executors.newFixedThreadPool((int) workerSpinner.getValue());
+        mandelbrotPanel.repaint();
+    }
+    private void setBlackBackground() {
+        getContentPane().setBackground(Color.BLACK);
+
+        // Utiliza SwingUtilities.invokeLater() para actualizar la interfaz gráfica
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Timer timer = new Timer(1000, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                        getContentPane().setBackground(Color.WHITE);
+                        repaintMandelbrot();
+                    }
+                });
+
+                timer.setRepeats(false); // Ejecutar una sola vez
+                timer.start();
+            }
+        });
+    }
+
+    private void repaintMandelbrot() {
+        int numWorkers = (int) workerSpinner.getValue();
+        int width = mandelbrotPanel.getWidth();
+        int height = mandelbrotPanel.getHeight();
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        // Restablece el fondo a blanco antes de repintar
+        getContentPane().setBackground(Color.WHITE);
+
+        System.out.println("Repintando el Mandelbrot con " + numWorkers + " trabajadores");
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                mandelbrotPanel.repaint();
+                repaintButton.setEnabled(true); // Habilita el botón de repintar nuevamente
+                isRepainting = false;
+            }
+        });
+    }
 }
--- src/main/java/org/example/MandelbrotWorker.java
+++ src/main/java/org/example/MandelbrotWorker.java
@@ -1,2 +1,48 @@
-package org.example;public class MandelbrotWorker {
+package org.example;
+
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Callable;
+
+class MandelbrotWorker implements Callable<Void> {
+    private BufferedImage image;
+    private int startY;
+    private int endY;
+
+    public MandelbrotWorker(BufferedImage image, int startY, int endY) {
+        this.image = image;
+        this.startY = startY;
+        this.endY = endY;
+    }
+ @Override
+ public Void call() throws Exception {
+     // Calcula el conjunto de Mandelbrot en la franja de startY a endY
+     for (int y = startY; y < endY; y++) {
+         for (int x = 0; x < image.getWidth(); x++) {
+             // Realiza los cálculos del conjunto de Mandelbrot para (x, y)
+             // y establece el color en la imagen
+             // (debes implementar esta parte según el algoritmo de Mandelbrot)
+             double zx = 0;
+             double zy = 0;
+             double cX = (x - 400) / 200.0;
+             double cY = (y - 300) / 200.0;
+             int iter = 0;
+             int maxIter = 570;
+             while (zx * zx + zy * zy < 4 && iter < maxIter) {
+                 double tmp = zx * zx - zy * zy + cX;
+                 zy = 2.0 * zx * zy + cY;
+                 zx = tmp;
+                 iter++;
+             }
+             if (iter < maxIter) {
+                 int color = 0x10000 * iter / maxIter;
+                 image.setRGB(x, y, color);
+             } else {
+                 image.setRGB(x, y, 0);
+             }
+
+         }
+     }
+     return null;
+ }
+
 }
